7c0474957cc5d667097c49f23dbc83c0746ff87f
ezno12 <bassembenyhia12@gmail.com>
2022-12-01 10:03:38 +0100

    football problem solved

--- football.py
+++ football.py
@@ -1,32 +1,9 @@
 #!/usr/bin/env python3
 """
 """
-listInput = []
-listInput[:] = input()
-zeroCount = 0
-oneCount = 0
-dangrous = False
-for i in range(len(listInput) - 1):
-    if listInput[i] == '0':
-        if listInput[i + 1] == '1' and zeroCount == 7:
-            dangrous = True
-            break
-        if listInput[i + 1] == '1' and zeroCount < 7:
-            zeroCount = 0
-        else:
-            zeroCount += 1
-            continue
-    if listInput[i] == "1":
-        
-        if listInput[i + 1] == '0' and oneCount == 7:
-            dangrous = True
-            break
-        if listInput[i + 1] == '0' and oneCount < 7:
-            oneCount = 0
-        else:
-            oneCount += 1
-            continue
-if dangrous == True:
-    print("YES")
+
+Input = input()
+if (('1' * 7) in Input or ('0' * 7) in Input):
+    print('YES')
 else:
-    print("NO")
+    print('NO')
